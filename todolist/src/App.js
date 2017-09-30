b9934a03b1a7b96b6cf0f680bf92cc68ce1c4932
Hamadoz <ghemid_m_dz@yahoo.fr>
2017-09-30 14:59:49 +0200

    todolist-done

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,18 +1,116 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Input, Button, Row, Col, ListGroup, ListGroupItem, Alert } from 'reactstrap'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      inputValue: '',
+      listToDo: [],
+      errorVisible: false,
+      visible: true
+    }
+
+    this.handleClickAdd = this.handleClickAdd.bind(this)
+    this.handleChangeInput = this.handleChangeInput.bind(this)
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  /**
+   * When canceling Alert notification
+   */
+  onDismiss() {
+    this.setState({ visible: false });
+  }
+
+  /**
+   * When clicking to Add Button
+   * Comparing if the element exists before adding it to the list
+   * If the element exists we show error message
+   */
+  handleClickAdd() {
+
+    if (!this.state.listToDo.includes(this.state.inputValue)) {
+      let list = this.state.listToDo
+      list.push(this.state.inputValue)
+
+      this.setState({
+        listToDo: list
+      })
+
+    }
+    else {
+      this.setState({
+        errorVisible: true
+      })
+    }
+
+  }
+
+  /**
+   * This function update the input value when the input is changing
+   * @param {*} event  
+   */
+  handleChangeInput(event) {
+    this.setState({
+      inputValue: event.target.value,
+      errorVisible: false
+    })
+  }
+
+
   render() {
+
+    /**
+     * Mapping the array and modifying it's elements
+     */
+    var list = this.state.listToDo.map((element) =>
+      <ListGroupItem>
+        {element}
+      </ListGroupItem>
+    )
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <br />
+        <Row>
+          <Col md={{ size: 6, offset: 3 }}>
+            <h3>To do list</h3>
+          </Col>
+        </Row>
+        <hr />
+        <br />
+        <ListGroup>
+          <Col md={{ size: 6, offset: 3 }}> {list} </Col>
+        </ListGroup>
+        <br />
+        <Row>
+          <Col md={{ size: 4, push: 3 }}>
+            <Input type="text" value={this.state.inputValue} onChange={this.handleChangeInput} />
+          </Col>
+          <br />
+          <Col md={{ size: 1, push: 3 }}>
+            <Button color='success' onClick={this.handleClickAdd} >
+              <img src="https://1001freedownloads.s3.amazonaws.com/icon/thumb/204/add-512.png" height="20" width="20" />
+              &nbsp;
+              Ajouter
+            </Button>
+          </Col>
+        </Row>
+        <br />
+        {this.state.errorVisible ? 
+            <Row>
+              <Col md={{ size: 6, offset: 3 }}>
+                <Alert color="danger" isOpen={this.state.visible} toggle={this.onDismiss}>
+                  todo existe déjà !
+                </Alert>
+              </Col>
+            </Row>
+          : null}
       </div>
     );
   }
